d99c50e9c966927bb604fd514785b00682448d72
Emmanouil Gkoulias <gkouem313@outlook.com>
2020-01-08 00:25:16 +0200

    All 3 radio buttons working, have to fix the colors

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
-const container = document.querySelector('#container');
 let size = 16;
 let div = [];
+let color = 'black';
 
+const container = document.querySelector('#container');
 const create = function(size) {
     for (let i = 0; i < size * size; i ++) {
         div[i] = document.createElement('div');
@@ -9,11 +10,25 @@ const create = function(size) {
         div[i].style.width = `calc(100% / ${size})`;
         div[i].style.height = `calc(100% / ${size})`;
         div[i].addEventListener('mouseover', event => {
-            div[i].style.backgroundColor = 'violet';
+            div[i].style.backgroundColor = color;
         });
         container.appendChild(div[i]);
     };
 }
+
+const inputs = Array.from(document.querySelectorAll('input'));
+document.addEventListener('click', event => {
+    let x = document.querySelector('input[name="color"]:checked');
+    if (x.value == 'black') {
+        color = 'black';
+    }
+    else if (x.value == 'rgb') {
+        color = 'violet';
+    }
+    else {
+        color = 'grey';
+    }
+});
 
 const reset = document.querySelector('#reset');
 reset.addEventListener('click', event => {
@@ -21,7 +36,6 @@ reset.addEventListener('click', event => {
         div[i].style.backgroundColor = 'white';
         container.removeChild(div[i]);
     }
-    console.log(container);
     size = prompt('How many squares per sides to you want me to make the new grid?');
     div = [];
     create(size);
